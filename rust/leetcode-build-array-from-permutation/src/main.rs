53fb91d81a2599ec7ca1df5421c7c9c78d254c57
Yumi Izumi <me@yumiizumi.com>
2024-05-22 16:00:54 +0900

    build-array-from-permutation

---
+++ rust/leetcode-build-array-from-permutation/src/main.rs
@@ -0,0 +1,13 @@
+struct Solution;
+
+impl Solution {
+    pub fn build_array(nums: Vec<i32>) -> Vec<i32> {
+        let mut ans = Vec::new();
+        for i in 0..nums.len() {
+            ans.push(nums[nums[i] as usize]);
+        }
+        ans
+    }
+}
+
+fn main() {}
